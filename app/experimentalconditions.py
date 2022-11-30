0ec7f668e2ed18a0730b03413e12350c61abd332
damian0604 <d.c.trilling@uva.nl>
2022-11-30 17:13:57 +0100

    allow calling select_recommender for another user than the current one

--- app/experimentalconditions.py
+++ app/experimentalconditions.py
@@ -80,9 +80,10 @@ def assign_group(force_equal_size=True):
     
 
 
-def select_recommender():
+def select_recommender(group=None):
     '''Determine the recommender for the experimental condition a user is in. Change this function to reflect your experimental design.'''
-    group = current_user.group
+    if not group:
+        group = current_user.group
     if(group == 1):
         # RANDOM SELECTION WITH GAMIFICATION
         method = rec.random_selection()
