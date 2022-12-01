e5f730de3020599b2804f2a3dec78933662ccf1c
damian0604 <d.c.trilling@uva.nl>
2022-12-01 14:20:08 +0100

    lazy_gettext() instead of gettext() in forms needed

--- app/forms.py
+++ app/forms.py
@@ -3,30 +3,30 @@ from wtforms import StringField, PasswordField, BooleanField, RadioField, Select
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, InputRequired, Length, NumberRange, NoneOf
 from app.models import User
 from werkzeug.security import generate_password_hash
-from flask_babel import gettext
+from flask_babel import lazy_gettext
 
 class LoginForm(FlaskForm):
-    username = StringField(gettext('Username'), validators = [DataRequired()])
-    password = PasswordField(gettext('Password'), validators = [DataRequired()])
-    remember_me = BooleanField(gettext('Stay logged on'))
-    submit = SubmitField(gettext('Login'))
+    username = StringField(lazy_gettext('Username'), validators = [DataRequired()])
+    password = PasswordField(lazy_gettext('Password'), validators = [DataRequired()])
+    remember_me = BooleanField(lazy_gettext('Stay logged on'))
+    submit = SubmitField(lazy_gettext('Login'))
 
 class RegistrationForm(FlaskForm):
-    username = StringField(gettext('Username'), validators = [DataRequired()])
-    email = StringField(gettext('Email'), validators = [DataRequired(), Email()])
-    password = PasswordField(gettext('Password'), validators = [DataRequired()])
-    password2 = PasswordField(gettext('Repeat password'), validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField(gettext('Register'))
+    username = StringField(lazy_gettext('Username'), validators = [DataRequired()])
+    email = StringField(lazy_gettext('Email'), validators = [DataRequired(), Email()])
+    password = PasswordField(lazy_gettext('Password'), validators = [DataRequired()])
+    password2 = PasswordField(lazy_gettext('Repeat password'), validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField(lazy_gettext('Register'))
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError(gettext('This username is already in use. Please choose a different one.'))
+            raise ValidationError(lazy_gettext('This username is already in use. Please choose a different one.'))
 
     def validate_email(self, email):
         user = User.query.filter_by(email_contact=email.data).first()
         if user is not None:
-            raise ValidationError(gettext('This email address is already in use. You probably already have registered.'))
+            raise ValidationError(lazy_gettext('This email address is already in use. You probably already have registered.'))
 
 class MultiCheckboxField(SelectMultipleField):
     option_widget = widgets.CheckboxInput()
@@ -48,82 +48,82 @@ class ChecklisteForm(FlaskForm):
         return True
 
 class FinalQuestionnaireForm(FlaskForm):
-    eval_diversity = SelectField(label=gettext(
+    eval_diversity = SelectField(label=lazy_gettext(
         "How diverse were the items that this site showed to you?"), validators=[DataRequired(), NoneOf(["-999"])],
         choices = [
-        (-999, gettext("--please make a choice--")),
-        (-3, gettext("-3 (not at all)")), 
+        (-999, lazy_gettext("--please make a choice--")),
+        (-3, lazy_gettext("-3 (not at all)")), 
         (-2, -2), 
         (-1, -1),
         (0, 0),
         (1, 1),
         (2, 2),
-        (3, gettext("+3 (very much)"))])
-    eval_personalization = SelectField(label=gettext(
+        (3, lazy_gettext("+3 (very much)"))])
+    eval_personalization = SelectField(label=lazy_gettext(
         "How aligned with your interests and preferences were the items that this site showed to you?"), validators=[DataRequired(), NoneOf(["-999"])],
         choices = [
-        (-999, gettext("--please make a choice--")),
-        (-3, gettext("-3 (not at all)")), 
+        (-999, lazy_gettext("--please make a choice--")),
+        (-3, lazy_gettext("-3 (not at all)")), 
         (-2, -2), 
         (-1, -1),
         (0, 0),
         (1, 1),
         (2, 2),
-        (3, gettext("+3 (very much)"))])
-    comments = TextAreaField(label=gettext("Is there anything you'd like to say to us?"))
-    submit = SubmitField(gettext("Done!"))
+        (3, lazy_gettext("+3 (very much)"))])
+    comments = TextAreaField(label=lazy_gettext("Is there anything you'd like to say to us?"))
+    submit = SubmitField(lazy_gettext("Done!"))
 
 class IntakeForm(FlaskForm):
     # remember to change the user class in models.py if you change sth here.
     # remember to check the /activate function in routes.py whether all fields are stored
-    age = IntegerField(label=gettext("How old are you?"), validators=[DataRequired(), NumberRange(min=18, max=120)])
-    gender = RadioField(label=gettext("What is your gender?"), validators=[DataRequired()], choices=[
-        gettext("female"), 
-        gettext("male"), 
-        gettext("non-binary"),
-        gettext("prefer not to say")])
-    education = SelectField(label=gettext("What is your the highest degree you have completed or are enrolled in?"), validators=[DataRequired(),NoneOf(["-999"])], choices=[
-        (-999, gettext("--please make a choice--")),
-        (0, gettext("no higher education")), 
-        (1, gettext("bachelor")), 
-        (2, gettext("master")),
-        (3, gettext("PhD"))])
-    newsinterest = SelectField(label=gettext(
+    age = IntegerField(label=lazy_gettext("How old are you?"), validators=[DataRequired(), NumberRange(min=18, max=120)])
+    gender = RadioField(label=lazy_gettext("What is your gender?"), validators=[DataRequired()], choices=[
+        lazy_gettext("female"), 
+        lazy_gettext("male"), 
+        lazy_gettext("non-binary"),
+        lazy_gettext("prefer not to say")])
+    education = SelectField(label=lazy_gettext("What is your the highest degree you have completed or are enrolled in?"), validators=[DataRequired(),NoneOf(["-999"])], choices=[
+        (-999, lazy_gettext("--please make a choice--")),
+        (0, lazy_gettext("no higher education")), 
+        (1, lazy_gettext("bachelor")), 
+        (2, lazy_gettext("master")),
+        (3, lazy_gettext("PhD"))])
+    newsinterest = SelectField(label=lazy_gettext(
         "How interested are you in news and current affairs?"), validators=[DataRequired(), NoneOf(["-999"])],
         choices = [
-        (-999, gettext("--please make a choice--")),
-        (-3, gettext("-3 (not at all)")), 
+        (-999, lazy_gettext("--please make a choice--")),
+        (-3, lazy_gettext("-3 (not at all)")), 
         (-2, -2), 
         (-1, -1),
         (0, 0),
         (1, 1),
         (2, 2),
-        (3, gettext("+3 (very much)"))])
-    polorient = SelectField(label=gettext(
+        (3, lazy_gettext("+3 (very much)"))])
+    polorient = SelectField(label=lazy_gettext(
         "In politics, we often talk about left or right. On a scale from -5 (left) to +5 (right), where would you place yourself?'"),
         validators=[DataRequired(), NoneOf(["-999"])],
-        choices = [(-999, gettext("--please make a choice--")),
-        (-5, gettext("-5 (left)")), 
+        choices = [(-999, lazy_gettext("--please make a choice--")),
+        (-5, lazy_gettext("-5 (left)")), 
         (-4, -4), 
         (-3, -3), 
         (-2, -2), 
         (-1, -1),
-        (0, gettext ("0 (center)")),
+        (0, lazy_gettext ("0 (center)")),
         (1, 1),
         (2, 2),
         (3, 3),
         (4, 4),
-        (5, gettext("+5 (right)"))])
-    submit = SubmitField(gettext("Done!"))
+        (5, lazy_gettext("+5 (right)"))])
+    submit = SubmitField(lazy_gettext("Done!"))
 
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField(gettext('Email'), validators=[DataRequired(), Email()])
-    submit = SubmitField(gettext('Reset my password!'))
+    email = StringField(lazy_gettext('Email'), validators=[DataRequired(), Email()])
+    submit = SubmitField(lazy_gettext('Reset my password!'))
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField(gettext('Password'), validators= [DataRequired()])
-    password2 = PasswordField(gettext('Repeat password'), validators = [DataRequired(), EqualTo('password')])
-    submit = SubmitField(gettext('Reset my password!'))
+    password = PasswordField(lazy_gettext('Password'), validators= [DataRequired()])
+    password2 = PasswordField(lazy_gettext('Repeat password'), validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField(lazy_gettext('Reset my password!'))
 
 
 class rating(FlaskForm):
@@ -131,12 +131,12 @@ class rating(FlaskForm):
     rating2 = TextAreaField()
 
 class ContactForm(FlaskForm):
-    email = TextAreaField(gettext('Your email: '))
-    lead = TextAreaField(gettext('Subject: '), validators = [DataRequired()])
-    message = TextAreaField(gettext('Your message:'), validators = [DataRequired()])
-    submit = SubmitField(gettext('Submit'))
+    email = TextAreaField(lazy_gettext('Your email: '))
+    lead = TextAreaField(lazy_gettext('Subject: '), validators = [DataRequired()])
+    message = TextAreaField(lazy_gettext('Your message:'), validators = [DataRequired()])
+    submit = SubmitField(lazy_gettext('Submit'))
 
 class ReportForm(FlaskForm):
     lead = TextAreaField('subject::', validators = [DataRequired()])
-    message = TextAreaField(gettext('What problem does this article have?'), validators = [DataRequired()])
-    submit = SubmitField(gettext('Submit'))
+    message = TextAreaField(lazy_gettext('What problem does this article have?'), validators = [DataRequired()])
+    submit = SubmitField(lazy_gettext('Submit'))
