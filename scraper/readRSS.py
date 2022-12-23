34a0a90202c2fca150e99fc5f3c7a9c2d187b3ad
damian0604 <d.c.trilling@uva.nl>
2022-12-23 18:44:43 +0100

    removed native sql connect dependency from readRSS

--- scraper/readRSS.py
+++ scraper/readRSS.py
@@ -8,7 +8,11 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 # ... which we need for this:
-from dbConnect import dbconnection
+#from dbConnect import dbconnection
+from config import Config
+
+from app.models import Articles
+
 from datetime import datetime
 from dateutil import parser
 import os
@@ -32,83 +36,16 @@ from requests import ConnectionError
 from mysql.connector import Error as MysqlError
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import text
 
 
-def readFeed(rssUrl, publisher, topic, lang, dbCursor,dbConnection):
-    try:
-        newsFeed = feedparser.parse(rssUrl)
-
-        tz = pytz.timezone('Canada/Eastern')
-        now = datetime.now(tz)
-
-        sql = "SELECT url FROM articles"
-        dbCursor.execute(sql)
-        articles = dbCursor.fetchall()
-
-        # CHECK TO SEE IF THESE ENTRIES ARE ALREADY IN DB
-        urlsInDb = []
-
-        for article in articles:
-            urlsInDb.append(article[0])
-
-        urlsFromFeed = []
-
-        for article in newsFeed.entries:
-            cleanUrl = article.link.split("?")[0]
-            urlsFromFeed.append(cleanUrl)
-
-        remainingUrls = list(set(urlsFromFeed) - set(urlsInDb))
-
-        count = 0
-
-        for article in newsFeed.entries:
-
-            # remove any html and leading spaces from description
-
-            print("The title is : {}".format(article.title))
-            print("The url is : {}".format(article.link))
-
-            cleanUrl = article.link.split("?")[0]
-
-            if(cleanUrl in remainingUrls):
-
-                print("INSERTING INTO DATABASE")
-
-                body,image,publishDate = readBody(article.link)
-
-                print("The publishDate is {}".format(publishDate))
-
-                dateTimeSQL = publishDate.strftime("%Y-%m-%d %H:%M:%S")
-
-                sql = "INSERT INTO articles(title, teaser, text, publisher, topic, url, date, imageUrl,imageFilename,lang) VALUES (%s, %s,%s,%s,%s,%s, %s,%s,%s,%s)"
-                values = (article.title, "testTeaser", body, publisher, topic, cleanUrl, dateTimeSQL, image,"",lang)
-
-                try:
-                    dbCursor.execute(sql, values)
-                    dbConnection.commit()
-
-                    sql2 = "INSERT INTO all_news (id) VALUES (%s)"
-                    values = (dbCursor.lastrowid,)
-                    dbCursor.execute(sql2, values)
-                    dbConnection.commit()
-
-                except MysqlError as err:
-                    print(err)
-                    print("Error Code:", err.errno)
-                    print("SQLSTATE", err.sqlstate)
-                    print("Message", err.msg)
-
-            else:
-                print("ALREADY IN DATABASE")
-    except Exception as error:
-        print("ERROR: {}".format(error))
 
 
 def readBody(url):
-
     body = "ERROR"
     image = ""
-
     try:
         article = Article(url,keep_article_html=True)
         article.download()
@@ -120,7 +57,6 @@ def readBody(url):
     except Exception as error:
         print (error)
         body = ""
-
     return(body, image,publishDate)
 
 def makeDatetime(timeString):
@@ -129,88 +65,201 @@ def makeDatetime(timeString):
     dateTimeSQL = dateTimePython.strftime("%Y-%m-%d %H:%M:%S")
     return(dateTimeSQL)
 
-def imageProcess(dbCursor, dbConnection):
 
-    sql = "SELECT id, imageUrl FROM articles WHERE imageFilename=''"
-    dbCursor.execute(sql)
-    images = dbCursor.fetchall()
 
-    for image in images:
 
-        if image[1] == "":
-            continue
+class Scraper():
+    def __init__(self, sqlalchemy_database_uri, rssUrl, publisher, topic, lang):
+        self.db = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+        self.rssUrl = rssUrl
+        self.publisher = publisher
+        self.topic = topic
+        self.lang = lang
 
-        try:
-            response = requests.get(image[1])
-        except Exception as error:
-            print (error)
-            continue
-
-        try:
-            img = Image.open(BytesIO(response.content))
-        except Exception as error:
-            print (error)
-            continue
-
-        if(img.format == "PNG"):
-            ext = "png"
-        else:
-            ext = "jpg"
+    def readFeed(self):
+        with Session(self.db) as session:
             try:
-                img = img.convert('RGB')
+                newsFeed = feedparser.parse(self.rssUrl)
+                # commented out to not enforce timezone
+                # tz = pytz.timezone('Canada/Eastern')
+                # now = datetime.now(tz)
+                now = datetime.now()
+
+                sql = "SELECT url FROM articles"
+                articles = session.execute(sql).fetchall()
+
+                # CHECK TO SEE IF THESE ENTRIES ARE ALREADY IN DB
+                urlsInDb = []
+
+                for article in articles:
+                    urlsInDb.append(article[0])
+
+                urlsFromFeed = []
+
+                for article in newsFeed.entries:
+                    cleanUrl = article.link.split("?")[0]
+                    urlsFromFeed.append(cleanUrl)
+
+                remainingUrls = list(set(urlsFromFeed) - set(urlsInDb))
+
+                count = 0
+
+                for article in newsFeed.entries:
+
+                    # remove any html and leading spaces from description
+
+                    print("The title is : {}".format(article.title))
+                    print("The url is : {}".format(article.link))
+
+                    cleanUrl = article.link.split("?")[0]
+
+                    if(cleanUrl in remainingUrls):
+
+                        print("INSERTING INTO DATABASE")
+
+                        body,image,publishDate = readBody(article.link)
+
+                        print("The publishDate is {}".format(publishDate))
+
+                        dateTimeSQL = publishDate.strftime("%Y-%m-%d %H:%M:%S")
+
+                        # sql = "INSERT INTO articles(title, teaser, text, publisher, topic, url, date, imageUrl,imageFilename,lang) VALUES (%s, %s,%s,%s,%s,%s, %s,%s,%s,%s)"
+                        # sql = "INSERT INTO articles(title, teaser, text, publisher, topic, url, date, imageUrl,imageFilename,lang) VALUES (%s, %s,%s,%s,%s,%s, %s,%s,%s,%s)"
+
+                        #values = (article.title, "testTeaser", body, self.publisher, self.topic, cleanUrl, dateTimeSQL, image,"",self.lang)
+
+                        try:
+                            #result = session.execute(text(sql), values)
+                            #session.commit()
+
+                            _art = Articles(title=article.title,
+                                teaser= "testTeaser",
+                                text = body,
+                                publisher = self.publisher,
+                                topic = self.topic, 
+                                url = cleanUrl, 
+                                date = dateTimeSQL, 
+                                imageUrl = image,
+                                imageFilename = "",
+                                lang = self.lang)
+
+                            result = session.add(_art)
+                            session.flush()
+                            lastrowid = _art.id
+                            print(f"ARTICLE ID IS ={lastrowid}")
+
+                            #sql2 = "INSERT INTO all_news (id) VALUES (%s)"
+                            #values = (result.lastrowid,)
+                            #session.execute(sql2, values)
+                            #session.commit()
+                            session.execute(f"INSERT INTO all_news (id) VALUES ({lastrowid})")
+                            session.commit()
+
+
+                        except MysqlError as err:
+                            print(err)
+                            print("Error Code:", err.errno)
+                            print("SQLSTATE", err.sqlstate)
+                            print("Message", err.msg)
+
+                    else:
+                        print("ALREADY IN DATABASE")
             except Exception as error:
-                print (error)
+                print("ERROR: {}".format(error))
 
-        try:
-            img.thumbnail((1280, 720))
-        except Exception as error:
-            print (error)
 
-        """
+class ImageProcessor():
+    def __init__(self, sqlalchemy_database_uri):
+        self.db = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 
-        width, height = img.size
+    def process_all(self):
+        with Session(self.db) as session:
+            sql = "SELECT id, imageUrl FROM articles WHERE imageFilename=''"
+            images = session.execute(sql).fetchall()
 
-        new_width = 800
-        new_height = 400
+            for image in images:
 
-        left = (width - new_width)/2
-        top = (height - new_height)/2
-        right = (width + new_width)/2
-        bottom = (height + new_height)/2
+                if image[1] == "":
+                    continue
 
-        img = img.crop((left, top, right, bottom))
+                try:
+                    response = requests.get(image[1])
+                except Exception as error:
+                    print (error)
+                    continue
 
-        """
+                try:
+                    img = Image.open(BytesIO(response.content))
+                except Exception as error:
+                    print (error)
+                    continue
 
-        newFilename = "../app/static/images/thumb_{}.{}".format(image[0],ext)
+                if(img.format == "PNG"):
+                    ext = "png"
+                else:
+                    ext = "jpg"
+                    try:
+                        img = img.convert('RGB')
+                    except Exception as error:
+                        print (error)
 
-        shortFilename = "thumb_{}.{}".format(image[0],ext)
+                try:
+                    img.thumbnail((1280, 720))
+                except Exception as error:
+                    print (error)
 
-        try:
-            img.save(newFilename,img.format)
-            sql = "UPDATE articles SET imageFilename = %s WHERE id = %s"
-            values = (shortFilename, image[0])
-            dbCursor.execute(sql, values)
-            dbConnection.commit()
-            print("Downloaded and inserted image for article : {}".format(image[0]))
-        except Exception as error:
-            print (error)
+                """
+
+                width, height = img.size
+
+                new_width = 800
+                new_height = 400
+
+                left = (width - new_width)/2
+                top = (height - new_height)/2
+                right = (width + new_width)/2
+                bottom = (height + new_height)/2
+
+                img = img.crop((left, top, right, bottom))
+
+                """
+
+                newFilename = "../app/static/images/thumb_{}.{}".format(image[0],ext)
+
+                shortFilename = "thumb_{}.{}".format(image[0],ext)
+
+                try:
+                    img.save(newFilename,img.format)
+                    #sql = "UPDATE articles SET imageFilename = %s WHERE id = %s"
+                    #values = (shortFilename, image[0])
+                    #session.execute(sql, values)
+                    session.execute(f'UPDATE articles SET imageFilename = "{shortFilename}" WHERE id = "{image[0]}"')
+                    session.commit()
+                    print("Downloaded and inserted image for article : {}".format(image[0]))
+                except Exception as error:
+                    print (error)
 
 def main():
     configrss = ConfigParser()
 
     configFile = os.path.join(os.path.dirname(__file__), 'feedlist.cfg')
     configrss.read(configFile)
-    dbCursor, dbConnection = dbconnection
 
     # read all the rss feeds from the feedlist file
 
     for x in range(1, int(configrss["config"]["feedCount"]) + 1):
         feedString = "feed" + str(x)
-        readFeed(configrss[feedString]["url"],configrss[feedString]["publisher"], configrss[feedString]["topic"], configrss[feedString]["lang"], dbCursor, dbConnection)
+        scraper = Scraper(Config.SQLALCHEMY_DATABASE_URI,
+            configrss[feedString]["url"],
+            configrss[feedString]["publisher"],
+            configrss[feedString]["topic"],
+            configrss[feedString]["lang"])
+        scraper.readFeed()
+        #readFeed(configrss[feedString]["url"],configrss[feedString]["publisher"], configrss[feedString]["topic"], configrss[feedString]["lang"], dbCursor, dbConnection)
 
     # download and process the images
-    imageProcess(dbCursor, dbConnection)
+    imageprocessor = ImageProcessor(Config.SQLALCHEMY_DATABASE_URI)
+    imageprocessor.process_all()
 
 if __name__ == "__main__":
     main()
