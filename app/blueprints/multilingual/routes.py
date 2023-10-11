3551bdf9ce28b7ea6994e26f094d5cb224e1ec35
damian0604 <d.c.trilling@uva.nl>
2023-10-11 13:41:58 +0200

    show diagnostics on profile page

--- app/blueprints/multilingual/routes.py
+++ app/blueprints/multilingual/routes.py
@@ -663,7 +663,9 @@ def profile():
         select_customizations = select_customizations(),
         select_detailed_stats = select_detailed_stats(),
         # TODO the may_finalize is not used in the template yet - add info box to it
-        may_finalize = may_finalize())
+        may_finalize = may_finalize(),
+        diagnostics = f"group: {current_user.group}; mysterybox: {select_recommender().mysterybox}; recommender: {select_recommender()}",
+        )
 
 
 @multilingual.route('/invite', methods = ['GET', 'POST'])
