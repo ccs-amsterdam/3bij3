fc672f01d4c1175a12394baeda23f51b1ff1e821
damian0604 <d.c.trilling@uva.nl>
2023-10-26 18:56:57 +0200

    first implementation reading nudge

--- app/gamification.py
+++ app/gamification.py
@@ -27,13 +27,14 @@ def get_nudge():
         return nudge
 
     nudge = _get_sharing_nudge(nudge)
-    
+    if nudge.get('nudge') != 'yes':
+        nudge = _get_reading_nudge(nudge)
+    logger.debug(f"returning nudge {nudge}")
     return nudge
+
 
 def _get_sharing_nudge(nudge):
     '''adds sharing nudges to the nudge dict'''
-
-    # by now, we're sure we actually want to create a nudge
 
     sql = "SELECT COUNT(*) FROM share_data WHERE user_id = {} AND timestamp > DATE_SUB(NOW(), INTERVAL 24 HOUR)".format(current_user.id)
     nudge["sql"] = sql # is this even necessary, storing in the dict?
@@ -97,5 +98,40 @@ def _get_sharing_nudge(nudge):
                 resultset = db.session.execute(sql3)
                 results = resultset.mappings().all()
                 nudge['selectedArticle'] = results[0]
+
+    return nudge
+
+
+
+def _get_reading_nudge(nudge):
+    '''adds reading nudges to the nudge dict'''
+
+    # get all topics of all available articles
+    sql1 = "SELECT DISTINCT articles.topic FROM articles"
+    listArticleTopics = [e[0] for e in db.session.execute(sql1).fetchall()]
+
+    # get all topics of all the articles that the user has read WITHIN LAST 24 hours
+    sql2 = "SELECT DISTINCT articles.topic FROM news_sel INNER JOIN articles ON news_sel.news_id = articles.id WHERE news_sel.user_id={} AND news_sel.starttime > DATE_SUB(NOW(),INTERVAL 24 HOUR);".format(current_user.id)
+    
+    readArticleTopics = [e[0] for e in db.session.execute(sql2).fetchall()]
+
+    notreadArticleTopics = list(set(listArticleTopics) - set(readArticleTopics))
+    logger.debug(readArticleTopics,notreadArticleTopics)
+    # double check to make sure there are some not shared topics then create a nudge if they haven't receieved a topic nudge in the last 24 hours
+    if(len(notreadArticleTopics) > 0):
+        sql = "SELECT COUNT(*) FROM nudges WHERE user_id = {} AND nudgeType='topic_read' AND timestamp > DATE_SUB(NOW(), INTERVAL 24 HOUR)".format(current_user.id)
+        if db.session.execute(sql).scalar() == 0:
+            nudge["nudge"] = "yes"
+            nudge["type"] = "topic_read"
+            nudgeInfo = Nudges(user_id=current_user.id,nudgeType="topic_read")
+            db.session.add(nudgeInfo)
+            db.session.commit()
+
+            removeNoneTopics = [i for i in notreadArticleTopics if i is not None]
+            
+            sql3 = "SELECT * FROM articles WHERE date > DATE_SUB(NOW(), INTERVAL 24 HOUR) AND topic IN ('{}') ORDER BY RAND() LIMIT 1".format("','".join(removeNoneTopics))
+            resultset = db.session.execute(sql3)
+            results = resultset.mappings().all()
+            nudge['selectedArticle'] = results[0]
 
     return nudge
